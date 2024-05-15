ebc6fd3360a34b084e9d5b9d7b796a7d87561155
Isai Chaidez <isaichaidez@gmail.com>
2024-05-14 21:06:48 -0700

    added small alert when setting changes are made

--- src/Components/Settings/index.tsx
+++ src/Components/Settings/index.tsx
@@ -1,19 +1,36 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { SettingsContext } from '../../Context/Settings';
 
 function Settings() {
   const { pageItems, changePageItemLength, sort, changeSort, hideCompleted, changeHideCompleted } = useContext(SettingsContext);
+  const [alert, setAlert] = useState<string>('');
+
+  const handleChangePageItemLength = (value: number) => {
+    changePageItemLength(value);
+    setAlert('Items per page changed');
+  };
+
+  const handleChangeSort = (value: string) => {
+    changeSort(value);
+    setAlert('Sort order changed');
+  };
+
+  const handleChangeHideCompleted = (value: boolean) => {
+    changeHideCompleted(value);
+    setAlert('Hide completed changed');
+  };
 
   return (
     <form>
       <h2>Settings</h2>
+      {alert && <div>{alert}</div>}
       <label>
         <p>Items Per Page</p>
-        <input type="number" value={pageItems} onChange={(e) => changePageItemLength(parseInt(e.target.value))} />
+        <input type="number" value={pageItems} onChange={(e) => handleChangePageItemLength(parseInt(e.target.value))} />
       </label>
       <label>
         <p>Sort By</p>
-        <select value={sort} onChange={(e) => changeSort(e.target.value)}>
+        <select value={sort} onChange={(e) => handleChangeSort(e.target.value)}>
           <option value="difficulty">Difficulty</option>
           <option value="assignee">Assignee</option>
           <option value="text">Text</option>
@@ -23,7 +40,7 @@ function Settings() {
       <br />
       <label>
         <span>Hide Completed</span>
-        <input type="checkbox" checked={!hideCompleted} onChange={(e) => changeHideCompleted(e.target.checked)} />
+        <input type="checkbox" checked={!hideCompleted} onChange={(e) => handleChangeHideCompleted(e.target.checked)} />
       </label>
     </form>
   );
